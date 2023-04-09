b14f5a91dca4d031a65afafb87118543913d32db
dlwsdqdws <dlwsdqdws@gmail.com>
2023-04-10 02:39:13 +0800

    order service

---
+++ services/order_service.go
@@ -0,0 +1,47 @@
+package services
+
+import (
+	"pro-iris/datamodels"
+	"pro-iris/repositories"
+)
+
+type IOrderService interface {
+	GetOrderByID(int64) (*datamodels.Order, error)
+	DeleteOrderByID(int64) bool
+	UpdateOrder(*datamodels.Order) error
+	InsertOrder(*datamodels.Order) (int64, error)
+	GetAllOrder() ([]*datamodels.Order, error)
+	GetAllOrderInfo(map[int]map[string]string, error)
+}
+
+func NewOrderService(repository repositories.IOrderRepository) IOrderService {
+	return &OrderService{repository}
+}
+
+type OrderService struct {
+	OrderRepository repositories.IOrderRepository
+}
+
+func (o *OrderService) GetOrderByID(orderID int64) (order *datamodels.Order, err error) {
+	return o.OrderRepository.SelectByKey(orderID)
+}
+
+func (o *OrderService) DeleteOrderByID(orderID int64) bool {
+	return o.OrderRepository.Delete(orderID)
+}
+
+func (o *OrderService) UpdateOrder(order *datamodels.Order) error {
+	return o.OrderRepository.Update(order)
+}
+
+func (o *OrderService) InsertOrder(order *datamodels.Order) (int64, error) {
+	return o.OrderRepository.Insert(order)
+}
+
+func (o *OrderService) GetAllOrder() ([]*datamodels.Order, error) {
+	return o.OrderRepository.SelectAll()
+}
+
+func (o *OrderService) GetAllOrderInfo() (map[int]map[string]string, error) {
+	return o.OrderRepository.SelectAllWithInfo()
+}
