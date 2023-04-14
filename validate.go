9e27851af10ba800a94dff8ddaa2d43dd37c6419
dlwsdqdws <dlwsdqdws@gmail.com>
2023-04-15 02:06:10 +0800

    validate add identity verification

--- validate.go
+++ validate.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"pro-iris/common"
+	"pro-iris/encrypt"
 )
 
 // Unified verification filter
@@ -11,10 +13,44 @@ import (
 func Auth(w http.ResponseWriter, r *http.Request) error {
 	fmt.Println("run Auth function successfully")
 	// Add permission verification based on cookie
+	err := checkUserInfo(r)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-//func checkUserInfo(r *http.Request)
+// Identity verification
+func checkUserInfo(r *http.Request) error {
+	// get uid from cookie
+	uidCookie, err := r.Cookie("uid")
+	if err != nil {
+		return errors.New("unable to access uid from cookie")
+	}
+	// get encrypted userInfo from cookie
+	signCookie, err := r.Cookie("sign")
+	if err != nil {
+		return errors.New("unable to access sign from cookie")
+	}
+	// Decrypt sign
+	signByte, err := encrypt.DePwdCode(signCookie.Value)
+	if err != nil {
+		return errors.New("encrypted User information has been tampered with")
+	}
+
+	fmt.Println("Identity verification")
+	fmt.Println("uid：" + uidCookie.Value)
+	fmt.Println("decrypted sign：" + string(signByte))
+	if checkInfo(uidCookie.Value, string(signByte)) {
+		return nil
+	} else {
+		return errors.New("identity verification failed")
+	}
+}
+
+func checkInfo(checkStr string, signStr string) bool {
+	return checkStr == signStr
+}
 
 // Execute normal logic
 func Check(w http.ResponseWriter, r *http.Request) {
@@ -28,5 +64,5 @@ func main() {
 	filter.RegisterFilterUri("/check", Auth)
 	// 3. start service
 	http.HandleFunc("/check", filter.Handle(Check))
-	http.ListenAndServe(":8003", nil)
+	http.ListenAndServe(":8083", nil)
 }
