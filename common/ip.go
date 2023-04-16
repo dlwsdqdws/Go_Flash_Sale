5f4fd3194ad4b40f509b260e19d3265403bd95e8
dlwsdqdws <dlwsdqdws@gmail.com>
2023-04-16 11:09:25 +0800

    add ip acquisition module

---
+++ common/ip.go
@@ -0,0 +1,24 @@
+package common
+
+import (
+	"errors"
+	"net"
+)
+
+// get localhost ip
+
+func GetIntranetIp() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, address := range addrs {
+		if ipnet, check := address.(*net.IPNet); check && ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", errors.New("unable to access intranet ip address")
+}
